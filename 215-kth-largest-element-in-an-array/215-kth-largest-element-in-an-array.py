b9a6fdfc1e5fccd88b97561ae4597c18127fb3b4
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-15 16:11:09 -0700

    Time: 694 ms (13.36%), Space: 25.3 MB (6.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,42 +1,11 @@
 class Solution:
-    # def findKthLargest(self, nums: List[int], k: int) -> int:
-    def partition(self, arr, start, end):
-        pivot = arr[(start + end)//2]
-        i = start
-        j = end
-        while i <= j:
-            while arr[i] < pivot:
-                i += 1
-            while arr[j] > pivot:
-                j -= 1
-            if i <= j:
-                arr[i],arr[j] = arr[j],arr[i]
-                i += 1
-                j -= 1
-        return i
-
-    def quicksort(self, arr, start, end, k):
-        if start < end:
-            
-            part = self.partition(arr, start, end)
-            if part <= len(arr)-k:
-                self.quicksort(arr, part, end, k)
-            else:
-                self.quicksort(arr, start, part-1, k)
-        
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        if nums == None:
-            return None
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            self.quicksort(nums, 0, len(nums)-1, k)
-            return nums[len(nums)-k]
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        for i in range(len(nums)):
+            nums[i] *= -1
+        heapq.heapify(nums)
+        for _ in range(k-1):
+            heapq.heappop(nums)
+        return heapq.heappop(nums) * -1
         
         
         
