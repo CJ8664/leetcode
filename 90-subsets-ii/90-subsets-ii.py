bf62f2e0c17766ab89d750aaf86526c761a7312a
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-16 14:59:04 -0700

    Time: 43 ms (85.36%), Space: 14.2 MB (9.47%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -4,7 +4,7 @@ class Solution:
         result = []
         subset = []
         
-        def dfs(idx):
+        def backtrack(idx):
             # we have reached end of traveral
             if idx >= len(nums):
                 result.append(copy.copy(subset))
@@ -12,14 +12,14 @@ class Solution:
                            
             # pick the element
             subset.append(nums[idx])
-            dfs(idx + 1)
+            backtrack(idx + 1)
 
             # IF we decide dont pick the element, we should also not pick any
             # of it duplicates
             subset.pop()
             while idx < len(nums) - 1 and nums[idx] == nums[idx + 1]:
                 idx += 1
-            dfs(idx + 1) #  + 1 to hit the recursion base condition
+            backtrack(idx + 1) #  + 1 to hit the recursion base condition
             
-        dfs(0)
+        backtrack(0)
         return result
