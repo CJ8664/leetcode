0d90e9c5df6d5662f01a769bb768a7ceb257f1f6
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-15 17:46:34 -0700

    Time: 619 ms (13.58%), Space: 34.7 MB (6.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,41 +1,31 @@
-class Solution:
-    def partition(self, arr, start, end):
-        pivot = arr[(start + end)//2]
-        i = start
-        j = end
-        while i <= j:
-            while arr[i] < pivot:
-                i += 1
-            while arr[j] > pivot:
-                j -= 1
-            if i <= j:
-                arr[i],arr[j] = arr[j],arr[i]
-                i += 1
-                j -= 1
-        return i
-
-    def quicksort(self, arr, start, end, k):
-        if start < end:
-            
-            part = self.partition(arr, start, end)
-            if part <= len(arr)-k:
-                self.quicksort(arr, part, end, k)
-            else:
-                self.quicksort(arr, start, part-1, k)
-        
+class Solution:        
     def findKthLargest(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        if nums == None:
-            return None
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            self.quicksort(nums, 0, len(nums)-1, k)
-            return nums[len(nums)-k]
+        def partition(l, r):
+            p = nums[(l + r)//2]
+            while l <= r:
+                while nums[l] < p: l += 1
+                while nums[r] > p: r -= 1
+                if l <= r:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    l, r = l + 1, r - 1
+            return l
+        
+        def quicksort(start, end):
+            if start < end:
+                part = partition(start, end)
+                if part <= len(nums)-k:
+                    quicksort(part, end)
+                else:
+                    quicksort(start, part-1)
+        
+
+        quicksort(0, len(nums)-1)
+        return nums[len(nums)-k]
         
         
         
