d36415d6c6ba78d6bc4edd70249be2ffe624187d
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-17 08:43:47 -0700

    Time: 197 ms (66.23%), Space: 16 MB (38.37%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n_r, n_c = len(board), len(board[0])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        def mark_border_regions(r, c):
+            if not (0 <= r < n_r) or not (0 <= c < n_c):
+                return
+            if board[r][c] in {"X", "#"}:
+                return 
+
+            board[r][c] = "#"
+            for d_r, d_c in dirs:
+                mark_border_regions(r + d_r, c + d_c)
+        
+        # Find all the border conneected regions
+        for r in range(n_r):
+            mark_border_regions(r, 0)
+            mark_border_regions(r, n_c -1)
+        for c in range(n_c):
+            mark_border_regions(0, c)
+            mark_border_regions(n_r - 1, c)
+            
+        # Flip all non border regions and also restore border regions
+        for r in range(n_r):
+            for c in range(n_c):
+                if board[r][c] == "O": board[r][c] = "X"
+                if board[r][c] == "#": board[r][c] = "O"   
+            
+                
+        
