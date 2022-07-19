e9d6c63802648d3a763df431c69f07d5b66bc967
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-19 05:22:22 -0700

    Time: 65 ms (25.82%), Space: 13.8 MB (74.54%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,29 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """        
-        def set_ele(cord, val):
-            matrix[cord[0]][cord[1]] = val
-        
-        def get_ele(cord):
-            return matrix[cord[0]][cord[1]]
-        
-        def swap(top_left, top_right, bottom_right, bottom_left):
-            tl = get_ele(top_left)
-            tr = get_ele(top_right)
-            br = get_ele(bottom_right)
-            bl = get_ele(bottom_left)
-            
-            set_ele(top_left, bl)
-            set_ele(top_right, tl)
-            set_ele(bottom_right, tr)
-            set_ele(bottom_left, br)
-        
-        for row in range(len(matrix)//2):
-            for col in range(row, len(matrix) - 1 - row):
-                top_left = (row, col)
-                top_right = (col, len(matrix) - 1 - row)
-                bottom_right = (len(matrix) - 1 - row, len(matrix) - 1 - col)
-                bottom_left = (len(matrix) - 1 - col, row)
-                # print(top_left, top_right, bottom_right, bottom_left)
-                swap(top_left, top_right, bottom_right, bottom_left)
+        l, r = 0, len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+                
+                # save the topleft
+                topLeft = matrix[top][l + i]
+                
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+                
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+                
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+                
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+            r -= 1
+            l += 1
         
