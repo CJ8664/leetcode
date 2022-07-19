e9dcd8a100ab02de926a909247f82016bb2fed35
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-19 05:56:59 -0700

    Time: 49 ms (61.40%), Space: 13.8 MB (96.82%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -3,8 +3,12 @@ class Solution:
         seen_numbers = set()
         while n not in seen_numbers:
             seen_numbers.add(n)
-            n = sum(int(x)**2 for x in str(n))
-            if n == 1:
+            s = 0
+            while n > 0:
+                s += (n % 10)**2
+                n = n // 10
+            if s == 1:
                 return True
+            n = s
         return False
         
