cd2d28279a982858f2f3d9cb09ba6187e4fdc312
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-20 07:18:37 -0700

    Time: 243 ms (27.44%), Space: 13.9 MB (30.11%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,28 +1,19 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0":
+        if "0" in {num1, num2}:
             return "0"
         
-        short, long = (num1, num2) if len(num1) < len(num2) else (num2, num1)
-        result = [0 for _ in range(len(long) + len(short))]
-        i, j, c = 0, 0, 0
-        for s in short[::-1]:
-            j = i
-            for l in long[::-1]:
-                t = int(s) * int(l) + c
-                result[j], c, j = result[j] + t % 10, t // 10, j + 1
-            if c > 0:
-                result[j] += c
-            i, j, c = i + 1, 0, 0
-        if c > 0:
-            result[i] += c
-        c = 0
-        for i, x in enumerate(result):
-            result[i] = (x + c) % 10
-            c = (x + c) // 10 
-        while result[-1] == 0:
-            result.pop()
-        return "".join([str(x) for x in result[::-1]])
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                temp = int(num1[i1]) * int(num2[i2]) + res[i1 + i2]
+                res[i1 + i2] = temp % 10
+                res[i1 + i2 + 1] += temp // 10
+        end = 0
+        while res[-1] == 0:
+            res.pop()
+        return "".join([str(x) for x in res[::-1]])
                 
                 
                 
