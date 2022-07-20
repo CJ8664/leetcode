1ec9735357380533e6d5cec1a7c0f12e3f9e707e
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-20 08:13:10 -0700

    Time: 357 ms (84.57%), Space: 15.9 MB (56.82%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -17,12 +17,8 @@ class DetectSquares:
         for (x2, y2), c in self.xyx_pts.items():
             if (abs(x1-x2) != abs(y1-y2)) or (x1 == x2 and y1 == y2):
                 continue
-            if x2 > x1:
-                x3, y3 = x1, y2
-                x4, y4 = x2, y1
-            else:
-                x3, y3 = x2, y1
-                x4, y4 = x1, y2
+            x3, y3 = (x1, y2) if x2 > x1 else (x2, y1)
+            x4, y4 = (x2, y1) if x2 > x1 else (x1, y2)
             if (x3, y3) in self.xyx_pts and (x4, y4) in self.xyx_pts:
                 result += c * self.xyx_pts[(x3, y3)] * self.xyx_pts[(x4, y4)]
         return result
