e9de8a258107ecc154e5d0f2d9c7d8ddd049375f
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-21 05:42:37 -0700

    Time: 1259 ms (29.78%), Space: 27.9 MB (78.04%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,9 +3,11 @@ class Solution:
         max_so_far = nums[0]
         curr_sum = 0
         for num in nums:
-            if curr_sum < 0:
-                curr_sum = 0
-            curr_sum += num
+            # negative prefix sum is not going to increase my total sum
+            # so reset the left starting this num
+            # if curr_sum < 0:
+            #     curr_sum = 0
+            curr_sum = (curr_sum + num) if curr_sum > 0 else num
             max_so_far = max(max_so_far, curr_sum)
         return max_so_far
             
