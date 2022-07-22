190117d0768ede19e739a95b3091c5c50d07159e
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-22 08:16:17 -0700

    Time: 8085 ms (5.02%), Space: 15.6 MB (91.95%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize > 0:
+            return False
+        result = []
+        for _ in range(len(hand) // groupSize):
+            result.append([])
+        hand.sort()
+        for h in hand:
+            placed = False
+            for r in result:
+                if len(r) == groupSize:
+                    continue
+                if not r or h == r[-1] + 1:
+                    r.append(h)
+                    placed = True
+                    break
+            if not placed:
+                return False
+        print(result)
+        return True
+                
+                
+            
+        
