f4f4fc153bdbf1b8c9ba2bf48ef881ad6b10fd3a
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-23 09:10:00 -0700

    Time: 266 ms (6.57%), Space: 17.2 MB (80.37%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,12 +3,11 @@ class Solution:
         result = {}
         for s in strs:
             key = [0 for _ in range(26)]
-            for c in s:
+            for c in s: 
                 key[ord(c) - ord('a')] += 1
-            key = tuple(key)
-            if key in result:
-                result[key].append(s)
-            else:
-                result[key] = [s]
+            key = "".join([chr(ord("a") + i) + str(x) for i, x in enumerate(key) if x > 0])
+            if key not in result:
+                result[key] = []
+            result[key].append(s)
         return list(result.values())
         
