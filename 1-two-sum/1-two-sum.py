55277401bb5b01d4091e70dcdeabe0a2b379c81d
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-23 08:38:52 -0700

    Time: 98 ms (62.40%), Space: 15.2 MB (24.50%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pos = {}
+        for idx, num in enumerate(nums):
+            cand = target - num
+            if cand in pos:
+                return [pos[cand], idx]
+            pos[num] = idx
+        
+        
