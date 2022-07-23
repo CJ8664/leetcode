798fe1e026e96119ad7ff8661cbc4a1cc0571b5b
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-22 19:39:09 -0700

    Time: 93 ms (9.80%), Space: 13.8 MB (97.54%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        max_r_pos = defaultdict(int)
+        for i, c in enumerate(s):
+            max_r_pos[c] = max(max_r_pos[c], i)
+            
+        l, r = 0, 0
+        
+        result = []
+        while r < len(s):
+            start = l
+            while l <= r:
+                r = max(r, max_r_pos[s[l]])
+                l += 1
+            result.append(r - start + 1)
+            r = l
+        return result
+            
+            
+        
