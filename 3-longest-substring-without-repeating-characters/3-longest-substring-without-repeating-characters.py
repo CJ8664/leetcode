6725178cf370ec1c0384bd2dbb9e28697c252ade
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-24 16:38:48 -0700

    Time: 134 ms (30.27%), Space: 14.1 MB (49.30%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,10 +4,13 @@ class Solution:
         l, r = 0, 0
         max_substring = 0
         while r < len(s):
+            # If new character add it to the substring
             if s[r] not in seen_set:
                 seen_set.add(s[r])
                 r += 1
                 max_substring = max(max_substring, len(seen_set))
+            # remove the left part of the current substring till the 
+            # new char at 'r' can be added to form a new substring
             else:
                 while l < r and s[r] in seen_set:
                     seen_set.remove(s[l])
