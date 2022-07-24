e34a80d8367445528c746f9a313f4be4f4982412
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-24 10:56:41 -0700

    Time: 256 ms (19.47%), Space: 15.9 MB (91.18%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,5 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # For a given index find the largest wall
+        # on the left side and right side
         left_max = [0 for _ in range(len(height))] 
         right_max = [0 for _ in range(len(height))]
         
@@ -9,6 +11,11 @@ class Solution:
             right_max[r] = max(right_max[r + 1], height[r + 1])
             l += 1
             r -= 1
+        
+        # At any given index the area of water it determeined the min 
+        # height of the left and right wall. Also, since the index can
+        # have its height we have to subtract that from the min of 
+        # left and right wall
         res = 0
         for i in range(len(height)):
             max_h = min(left_max[i], right_max[i])
