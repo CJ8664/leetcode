3de3e71a99bfede897146ac6f69dd4f6a1079ab3
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-24 16:25:19 -0700

    Time: 1254 ms (75.87%), Space: 25 MB (37.97%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -2,6 +2,8 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_prefix = prices[0]
         max_profit = 0 
+        # To sell at the current price, what could have been the 
+        # lowest buy price on the left
         for price in prices:
             max_profit = max(price - min_prefix, max_profit)
             min_prefix = min(min_prefix, price)
