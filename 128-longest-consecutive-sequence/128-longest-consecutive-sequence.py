7942545de55f0f13d1156f73605d793e9df4cf95
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-24 07:10:18 -0700

    Time: 586 ms (54.58%), Space: 29 MB (20.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -6,11 +6,11 @@ class Solution:
         for num in num_set:
             # check if its the start of a sequence
             if (num - 1) not in num_set:
-                current_num = num
+                # current_num = num
                 current_streak = 1
 
-                while current_num + 1 in num_set:
-                    current_num += 1
+                while num + current_streak in num_set:
+                    # current_num += 1
                     current_streak += 1
 
                 longest_streak = max(longest_streak, current_streak)
