2380cf5c4726d36f4f1213cafa9a4a80a2c80607
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-26 19:27:58 -0700

    Time: 36 ms (83.53%), Space: 13.8 MB (70.03%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par_map = {
+            "}": "{", 
+            ")": "(",
+            "]": "[",
+        }
+        stack = deque()
+        for par in s:
+            if par not in par_map:
+                stack.append(par)
+            elif par in par_map and len(stack) > 0 and stack[-1] == par_map[par]:
+                stack.pop()
+            else:
+                return False
+        return len(stack) == 0
+                
+                
+        
