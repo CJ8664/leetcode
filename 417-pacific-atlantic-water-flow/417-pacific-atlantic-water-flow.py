252e8bae32bd7e37656ebf9a197a097c365ee81a
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-30 13:29:32 -0700

    Time: 308 ms (87.50%), Space: 15.6 MB (78.32%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,37 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        n_r, n_c = len(heights), len(heights[0])
-        pac, atl = set(), set()
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(heights), len(heights[0])
+        pac_plots, atl_plots = set(), set()
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         
-        def explore(r, c, explored, prev_height):
-            if not (0 <= r < n_r) or not (0 <= c < n_c) or (r, c) in explored:
-                return
-            if prev_height > heights[r][c]:
-                return
+        def search(r, c, prev_height, plots):
+            if not (0 <= r < ROWS) or not (0 <= c < COLS) or (r, c) in plots: return
+            if heights[r][c] < prev_height: return
             
-            # Mark current as explored
-            explored.add((r, c))
-            for d_r, d_c in dirs:
-                explore(r + d_r, c + d_c, explored, heights[r][c])
+            plots.add((r, c))            
+            for nr, nc in dirs:
+                search(r + nr, c + nc, heights[r][c], plots)
         
-        # For each row check elements
-        # - left column and travel inwards from pacific
-        # - right column and travel inwards from atlantic
-        for r in range(n_r):
-            explore(r, 0, pac, heights[r][0])
-            explore(r, n_c - 1, atl, heights[r][n_c - 1])
+        # Get plots which reach atlantic and pacific backwards
+        # from verticals
+        for r in range(ROWS):
+            search(r, 0, -1, pac_plots)
+            search(r, COLS - 1, -1, atl_plots)
             
-        # For each columns check elements
-        # - top row and travel inwards from pacific
-        # - right column and travel inwards from atlantic
-        for c in range(n_c):
-            explore(0, c, pac, heights[0][c])
-            explore(n_r - 1, c, atl, heights[n_r - 1][c])
+        # Get plots which reach atlantic and pacific backwards
+        # from verticals
+        for c in range(COLS):
+            search(0, c, -1, pac_plots)
+            search(ROWS - 1, c, -1, atl_plots)
             
-        # Return the set of elements that can be reached 
-        # from both pacific and atlantic
-        return [(r, c) for (r, c) in (pac & atl)]
-            
-        
+        return (pac_plots & atl_plots)
         
