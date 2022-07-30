b3a144bdfe9744378f830303b42bcb5c9102571c
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-30 12:31:03 -0700

    Time: 275 ms (99.03%), Space: 16.4 MB (80.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,24 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        result = 0
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         
-        def sink_island(r, c):
-            if not (0 <= r < len(grid)) or not (0 <= c < len(grid[0])):
+        def sink(r, c):
+            if not (0 <= r < ROWS) or not (0 <= c < COLS) or grid[r][c] != "1": 
                 return
-            if grid[r][c] == "0":
-                return 
-                
+            # sink the part of island
             grid[r][c] = "0"
-            for nr, nc in dir:
-                sink_island(r + nr, c + nc)
-        
-        num_isle = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+            for nr, nc in dirs:
+                sink(r + nr, c + nc)
+            
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == "1":
-                    num_isle += 1
-                    sink_island(r, c)
-                    
-        return num_isle
-                    
+                    result += 1
+                    sink(r, c)
+        return result
+
         
