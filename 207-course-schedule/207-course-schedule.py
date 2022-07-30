4da4a68f596ba95f91264504dcd49c5b193c7694
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-30 13:52:24 -0700

    Time: 181 ms (33.55%), Space: 18.1 MB (5.44%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        memory, visiting = set(), set()
+        pre_req_map = defaultdict(set)
+        
+        def check_prereq(c):
+            # If we have checked the course before
+            if c in memory:
+                return True
+            # This course has no pre-req
+            if not pre_req_map[c]:
+                return True
+            # Pre-req cycle
+            if c in visiting: 
+                return False
+
+            # Mark course as visiting
+            visiting.add(c)
+            
+            # Check all the pre-req of current course
+            for p in pre_req_map[c]:
+                if not check_prereq(p):
+                    return False
+                memory.add(p)
+            
+            # Remove current course as visiting so that other 
+            # courses can check it in their search
+            visiting.remove(c)
+            
+            return True
+        
+        for c, p in prerequisites:
+            pre_req_map[c].add(p)
+            
+        for c, _p in prerequisites:
+            if not check_prereq(c):
+                return False
+        return True
+        
