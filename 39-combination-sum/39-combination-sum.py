cc295135f29fc2c2750f3fcbc5543d38dce1bfd0
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-30 11:26:10 -0700

    Time: 135 ms (50.14%), Space: 14 MB (91.02%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,21 +1,36 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+        result = []
+        subset = []
+        
+        # Required for pruning logic of the DFS + Bactracking
+        candidates.sort()
+        
+        def dfs(curr_sum, curr_idx):
+            if curr_sum > target:
                 return
-            if i >= len(candidates) or total > target:
+            if curr_sum == target:
+                result.append(copy.copy(subset))
                 return
-
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i + 1, cur, total)
-
-        dfs(0, [], 0)
-        return res
+            
+            for idx in range(curr_idx, len(candidates)):
+                # Add the current element 
+                subset.append(candidates[idx])
+                # and try to see if we find a solution
+                dfs(curr_sum + candidates[idx], idx)
+                # Backtrack and see if there are other solution
+                subset.pop()
+                
+                # Pruning logic, if the current candidate is not 
+                # part of solution, all the elements after that
+                # are going to be bigger and hence not going to be
+                # part of solution as well
+                if curr_sum + candidates[idx] > target:
+                    break
+                    
+        dfs(0, 0)
+        return result
+                
                 
                 
             
