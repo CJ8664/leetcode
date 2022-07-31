e436e6d84051cc5eb64f78c68028cd4d261fff45
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 15:57:06 -0700

    Time: 2102 ms (60.78%), Space: 14.3 MB (42.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,11 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # There can be infinite ways to arrange a given amount 
+        # as a default value
         dp = [float('inf') for _ in range(amount + 1)]
+        # Minimum way to get to total 0 is 0 ( pick no coin )
         dp[0] = 0
+        coins.sort()
         for i in range(1, amount + 1):
             for c in coins:
+                # If i select this coin, what is the minimum number
+                # of ways to pick coins for remaining sum. If in the 
+                # coins list I find a minimum that is less than
+                # number of way for a previous coin then min with self
                 if i - c >= 0:
                     dp[i] = min(dp[i], dp[i - c] + 1)
+                else:
+                    break
         return dp[amount] if dp[amount] != float('inf') else -1
                     
             
