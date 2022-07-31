28ad362ebe241f4c86575c91c72ec899371aad6e
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 15:03:18 -0700

    Time: 55 ms (40.41%), Space: 14.2 MB (10.59%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -7,7 +7,8 @@ class Solution:
             if s[idx] == "0":
                 return 0
             res = helper(idx + 1)
-            if idx + 1 < len(s) and (s[idx] == "1" or (s[idx] == "2" and s[idx + 1] in "0123456")):
+            #  (s[idx] == "1" or (s[idx] == "2" and s[idx + 1] in "0123456"))
+            if idx + 1 < len(s) and int(s[idx:idx + 2]) <= 26:
                 res += helper(idx + 2)
             memory[idx] = res
             return res
