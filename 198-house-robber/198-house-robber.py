fffd937889ecdc3b7ff5f2de33d061d90520bbde
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 07:52:29 -0700

    Time: 56 ms (33.23%), Space: 13.8 MB (66.03%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,5 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        ########################
+        ###### DP approach #####
+        ########################
+        
+        # Rob 1 represents that you didn't rob last hous
+        # Rob 2 represents that you robbed last house
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            # For a given house, temp represents the max 
+            # you can rob which is 
+            # if you rob this and not the previous: n + rob1
+            # if you dont rob this since you robbed last: rob2
+            temp = max(n + rob1, rob2)
+            # If we didnt rob this house rob2 respresents rob1
+            rob1 = rob2
+            # If we did rob this house rob2 represent temp
+            rob2 = temp
+        # rob2 represents the maximum for last house
+        return rob2
+        
+        ########################
+        ## Backtrack approach ##
+        ########################
         memory = {}
         def backtrack(pos):
             if pos >= len(nums):
