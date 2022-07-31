a4b0035ce9f1d9a8ed2f226d77c3d5238ad50ba3
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 15:06:26 -0700

    Time: 40 ms (79.13%), Space: 13.9 MB (46.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,5 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # Dynamic Programming
+        dp = {len(s): 1}
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+
+            if i + 1 < len(s) and (
+                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
+            ):
+                dp[i] += dp[i + 2]
+        return dp[0]
+    
+    
+        # DFS + Memoization
         memory = {len(s): 1}
         def helper(idx):
             if idx in memory:
@@ -7,7 +23,6 @@ class Solution:
             if s[idx] == "0":
                 return 0
             res = helper(idx + 1)
-            #  (s[idx] == "1" or (s[idx] == "2" and s[idx + 1] in "0123456"))
             if idx + 1 < len(s) and int(s[idx:idx + 2]) <= 26:
                 res += helper(idx + 2)
             memory[idx] = res
