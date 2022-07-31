5e0d95cf06f2d53dbc1a8e51c9fd1329777fe749
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 15:52:30 -0700

    Time: 2210 ms (55.19%), Space: 14.4 MB (42.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,7 +2,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [float('inf') for _ in range(amount + 1)]
         dp[0] = 0
-        coins.sort()
+        # coins.sort()
         for i in range(1, amount + 1):
             for c in coins:
                 rem_sum = i - c
