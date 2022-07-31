70df9c96806a954bd6837e910c61f9057298b4ed
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-30 20:36:31 -0700

    Time: 148 ms (43.77%), Space: 13.9 MB (68.58%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,36 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2Count[ord(s2[i]) - ord("a")] += 1
-
-        matches = 0
-        for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
-
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
+        s1_counter = Counter(s1)
+        l, r = 0, 0
+        while r < len(s2):
+            if s2[r] in s1_counter: 
+                if s1_counter[s2[r]] > 0:
+                    s1_counter[s2[r]] -= 1
+                    r += 1
+                else:
+                    while s1_counter[s2[r]] == 0:
+                        if s2[l] in s1_counter:
+                            s1_counter[s2[l]] += 1
+                        l += 1
+            else:
+                r += 1
+                while l < r:
+                    if s2[l] in s1_counter:
+                        s1_counter[s2[l]] += 1
+                    l += 1
+                
+            if r - l == len(s1):
                 return True
-
-            index = ord(s2[r]) - ord("a")
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord("a")
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
-
-
+        return False
+            
+        
+        
