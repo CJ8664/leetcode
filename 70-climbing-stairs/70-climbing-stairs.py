0004420b20a4a642a74be197b440e6ca3e1958f2
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 07:29:14 -0700

    Time: 37 ms (78.28%), Space: 13.8 MB (57.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,9 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n < 3:
             return n
-        one_step, two_step = 1, 2
+        second_last_step, last_step = 1, 2
+        # For a given postion you can reach it by 
+        # 1 step + number of way you reached last step
+        # 2 step + number of way you reached second last step
+        # 1 and 2 are possible moves given in question hence the equation
         for _ in range(3, n + 1):
-            one_step, two_step = two_step, (one_step + two_step)
-        return two_step
+            second_last_step, last_step = last_step, (second_last_step + last_step)
+        return last_step
             
         
