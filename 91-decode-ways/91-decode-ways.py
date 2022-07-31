35fb2b1d7fc779da0b83a1cfd305812a157986bd
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 15:01:54 -0700

    Time: 40 ms (79.13%), Space: 14.2 MB (20.03%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -7,10 +7,8 @@ class Solution:
             if s[idx] == "0":
                 return 0
             res = helper(idx + 1)
-            if idx + 1 < len(s):
-                two_digit = int(s[idx]) * 10 + int(s[idx + 1])
-                if two_digit <= 26:
-                    res += helper(idx + 2)
+            if idx + 1 < len(s) and (s[idx] == "1" or (s[idx] == "2" and s[idx + 1] in "0123456")):
+                res += helper(idx + 2)
             memory[idx] = res
             return res
         return helper(0)
