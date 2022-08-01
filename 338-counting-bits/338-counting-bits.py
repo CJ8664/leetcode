41fdec3f780ea3a5330435900715e583a7cc5622
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-01 06:15:52 -0700

    Time: 109 ms (78.69%), Space: 20.8 MB (78.80%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,5 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # Trick way
+        # 6 --> 110, 13 --> 1101 --> 110-1
+        # so bits in 6 + lsb 
+        counter = [0]
+        for i in range(1, n+1):
+            counter.append(counter[i >> 1] + i % 2)
+        return counter
+        
+        # Loops and pattern way
         num_bits = [0 for _ in range(n + 1)]
         counter, i = 1, 1
         while i < len(num_bits):
