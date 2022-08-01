979828739da1def549caf3bba9606e0fcba5c920
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 21:35:52 -0700

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -2,6 +2,7 @@ import os
 
 for f in os.listdir("../"):
     path = f"../{f}"
+    print(f"Checking path {path}")
     if not os.path.isdir(path):
         continue
     if path == "../scripts":
@@ -10,9 +11,12 @@ for f in os.listdir("../"):
         if not code_file.endswith(".py"):
             continue
         full_file_name = path + "/" + code_file
+        print(f"Found python code: {full_file_name}")
         with open(full_file_name, "r") as fr:
             code = fr.read()
         with open(path + "/CODE.md", "w") as fw:
             fw.write(f"```python\n{code}\n```\n")
+        print(f"Wrote: {path}/CODE.md")
     if os.path.exists(path + "/NOTES.md"):
+        print(f"Removing: {path}/NOTES.md")
         os.remove(path + "/NOTES.md")
