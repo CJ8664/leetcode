bac8302e3c49a5b314f111b95f12001371f963d8
Chirag Jain <er.chiragjain92@gmail.com>
2022-07-31 18:37:16 -0700

    Time: 440 ms (35.93%), Space: 32.4 MB (87.16%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,20 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        stack = collections.deque()
-        stack.append((root, float('-inf')))
         result = 0
-        while stack:
-            curr_node, curr_max = stack.pop()
-            if curr_node.val >= curr_max:
+        def helper(node, curr_max):
+            nonlocal result
+            if not node:
+                return
+            if node.val >= curr_max:
                 result += 1
-            next_max = max(curr_max, curr_node.val)
-            if curr_node.right:
-                stack.append((curr_node.right, next_max))
-            if curr_node.left:
-                stack.append((curr_node.left, next_max))
+            next_max = max(curr_max, node.val)
+            helper(node.left, next_max)
+            helper(node.right, next_max)  
+        helper(root, float('-inf'))
         return result
-            
-            
         
+            
+            
+            
         
