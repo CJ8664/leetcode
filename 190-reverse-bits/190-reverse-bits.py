3333fdaa63ba9e1b4ef4368030f806f36e3061a0
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-01 06:29:14 -0700

    Time: 38 ms (85.10%), Space: 13.7 MB (94.15%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,5 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        # Get the LSB and keep appending to result.
+        # while result shifts to the left the lsb is 
+        # always appended as result's so over time by left 
+        # shift it becomes msb
         result = 0
         for _ in range(32):
             lsb = n & 1
