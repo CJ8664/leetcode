c8d09d2a1c2693f5520564b63cd756abf568a682
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-01 23:57:05 -0700

    Time: 57 ms (97.46%), Space: 14.1 MB (20.20%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,7 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         for i in range(2, len(cost)):
-            cost[i] += min(cost[i-2] + cost[i-1], cost[i-2], cost[i-1])
+            # To cost[i] represents the cost you need to pay to get to next
+            # step starting from i.
+            # But to get to step i, you need to pay some cost. 
+            # That is pay the cost from 
+            # - second last step and take two steps
+            # - last step and take one step
+            cost[i] += min(cost[i-2], cost[i-1])
         return min(cost[-1], cost[-2])
             
             
