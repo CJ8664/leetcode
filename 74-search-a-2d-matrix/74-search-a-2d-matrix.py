467949ade24f5b51c28c214f652fde7aeafdda3f
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-01 22:22:22 -0700

    Time: 49 ms (88.52%), Space: 14.5 MB (42.97%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Consider the 2D matrix to be a flat 1D array
         l, r = 0, len(matrix) * len(matrix[0]) - 1
         while l <= r:
             mid = (l + r) // 2
