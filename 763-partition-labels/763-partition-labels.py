417f2988ddf7fed9239722675f232d770acaffd8
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-02 22:45:39 -0700

    Time: 83 ms (19.47%), Space: 13.8 MB (68.45%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,13 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        max_pos = {}
-        for idx in range(len(s) - 1, -1, -1):
-            if s[idx] not in max_pos:
-                max_pos[s[idx]] = idx
+        # Store the rightmost idx where a particular element 
+        # can be found in the string
+        max_r_pos = {c:i for i, c in enumerate(s)}
+        # Left indicates the begining of the substring, Right
+        # indicates the current max right of substring
         l, r, idx = 0, 0, 0
         result = []
         while idx < len(s):
-            r = max(r, max_pos[s[idx]])
+            r = max(r, max_r_pos[s[idx]])
             if idx == r:
                 result.append(r - l + 1)
                 l = r + 1
