4d835e35078bc41ca5721349d09dbd9af1cdb7cd
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-03 06:33:26 -0700

    Time: 378 ms (5.01%), Space: 18.2 MB (51.66%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,9 +1,12 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # necessary so that we can compare the end of left with
+        # start of right interval
         intervals.sort()
         result = []
         for idx in range(len(intervals) - 1):
             left, right = intervals[idx], intervals[idx + 1]
+            # Left interval ends before the start of right interval
             if left[1] < right[0]:
                 result.append(left)
             else:
