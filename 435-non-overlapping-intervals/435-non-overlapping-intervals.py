cf45a972f0964067f013073ec8f0962bf24a5e60
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-03 19:44:09 -0700

    Time: 2199 ms (49.75%), Space: 50.1 MB (97.17%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -1,14 +1,15 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: x[0])
-        result, i = 0, 0
-        while i < len(intervals) - 1:
-            if intervals[i][1] > intervals[i + 1][0]:
+        result = 0
+        for idx in range(len(intervals) - 1):
+            left, right = intervals[idx], intervals[idx + 1]
+            # The left meeting ends after the start of right ( overlaps )
+            if left[1] > right[0]:
                 result += 1
-                intervals[i + 1][0] = min(intervals[i][0], intervals[i + 1][0])
-                intervals[i + 1][1] = min(intervals[i][1], intervals[i + 1][1])
-            i += 1
+                intervals[idx + 1][0] = min(left[0], right[0])
+                # drop the meeting that is longer as it may 
+                # overlap with other too
+                intervals[idx + 1][1] = min(left[1], right[1])
         return result
-                
-                
         
