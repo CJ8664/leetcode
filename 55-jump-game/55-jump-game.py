45925ff97fd6d295eba26a4d653ecd8d55b1baf8
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-03 19:56:51 -0700

    Time: 822 ms (41.80%), Space: 15.4 MB (6.23%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,15 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # Tracks the last position from the right side at which we can reach
-        # the last index of array
-        goal = len(nums) -1
+        # the last index of array. The new goal becomes that position
+        goal = len(nums) - 1
         for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= goal:
                 goal = i
         return goal == 0
-            
-                
-            
-                
-                
         
