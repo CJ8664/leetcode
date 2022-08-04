292c5619b30cf18b3341742e7e7f7d447f26aeea
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-03 19:26:19 -0700

    Time: 57 ms (30.23%), Space: 13.9 MB (73.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,7 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        row = [1 for _ in range(n)]
-        for _ in range(m - 1):
+        # Create the bottom row of the grid
+        row = [0 for _ in range(n)]
+        row[-1] = 0
+        for _ in range(m):
             new_row = [0 for _ in range(n)]
             for c in range(n - 1, -1, -1):
                 if c == n - 1:
