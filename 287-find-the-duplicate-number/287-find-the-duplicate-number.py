bb7380db57c9618c416805463f06cb05039b5a2c
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-04 06:29:55 -0700

    Time: 1165 ms (28.66%), Space: 28.1 MB (30.25%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,5 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # Marking 
+        for num in nums:
+            i = abs(num)
+            if nums[i] > 0:
+                nums[i] *= -1
+            # The num represents an index, and the value pointed by it
+            # is already makred negative. Means we are seeing num
+            # again and hence is the duplicate
+            else:
+                return i
+                
         # Floyd's Tortoise and Hare (Cycle Detection)
         slow, fast = 0, 0
         while True:
