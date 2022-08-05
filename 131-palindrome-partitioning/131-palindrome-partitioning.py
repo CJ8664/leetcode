22887fb7b56f2f78603c32f5b5c4c8eeadee6cbb
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-05 14:41:08 -0700

    Time: 893 ms (64.76%), Space: 30.1 MB (65.31%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,7 +1,7 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res, part = [], []
-        
+        # Check if the string bound by given indexes is palindrome
         def isPali(l, r):
             while l < r:
                 if s[l] != s[r]:
@@ -10,13 +10,19 @@ class Solution:
             return True
         
         def dfs(i):
+            # if we have reached the end of string to find partitions
             if i >= len(s):
                 res.append(part.copy())
                 return
+            # for i = 0 get substring 0:1, 0:2, 0:3...
+            # If that string is palindrome, recurisively check if any of 
+            # its substring is palindorme
             for j in range(i, len(s)):
                 if isPali(i, j):
                     part.append(s[i:j+1])
                     dfs(j + 1)
+                    # before this is poped recurison base comdtion will be hit
+                    # and the result will be preserved
                     part.pop()
         dfs(0)
         return res
