12294534f1e5adba428569f7a9fe92b51ca83132
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-05 14:57:40 -0700

    Time: 958 ms (57.97%), Space: 19.3 MB (54.17%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,13 +1,18 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # If total gas is less than total cost then we can never
+        # complete the circuit
         if sum(gas) < sum(cost):
             return -1
+        
         
         total, result = 0, 0
         for i, (g, c) in enumerate(zip(gas, cost)):
             total += g - c
             if total < 0:
                 total = 0
+                # If we cannot start the journey from i
+                # then we might be able to start from i+1
                 result = i + 1
         return result
                 
