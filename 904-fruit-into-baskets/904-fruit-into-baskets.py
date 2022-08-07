67588a9508bc5fc547de24c7c89b556f17d4c304
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-07 11:46:19 -0700

    Time: 1792 ms (19.19%), Space: 20.2 MB (40.09%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -4,9 +4,15 @@ class Solution:
         l, r = 0, 0 
         result = 1
         while r < len(fruits):
+            # Add current fruit to basket
             basket[fruits[r]] += 1
+            # If the number of distinct fruits in basket are greater 
+            # than 2 than remove the fruits from the left of the window
+            # till only 2 types of fruits remain in basket
             while len(basket) > 2:
                 basket[fruits[l]] -= 1
+                # if the count of this fruit type is 0, remove it from
+                # basket
                 if basket[fruits[l]] == 0: basket.pop(fruits[l])
                 l += 1
             result = max(result, r - l + 1)
