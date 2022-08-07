7cc7068981cb90b44397b9305c7eca66fc0a4048
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-07 16:01:11 -0700

    Time: 336 ms (99.33%), Space: 21.8 MB (71.77%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -3,8 +3,12 @@ class Solution:
         result = []
         for num in nums:
             idx = abs(num) - 1
+            # If we are trying to mark a number that is already 
+            # negative it means that we have seen that index 
+            # before and hence the number is duplicate
             if nums[idx] < 0:
                 result.append(idx + 1)
+            # Seeing index for first time, mark negative
             else:
                 nums[idx] *= -1
         return result
