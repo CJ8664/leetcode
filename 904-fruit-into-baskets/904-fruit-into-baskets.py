866826545b9262e00c2817ff394ef92fe53acfef
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-07 11:44:08 -0700

    Time: 2048 ms (8.58%), Space: 20.1 MB (58.94%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,20 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        current_basket = defaultdict(int)
+        basket = defaultdict(int)
         l, r = 0, 0 
         result = 1
         while r < len(fruits):
-            if len(current_basket) < 2 or fruits[r] in current_basket:
-                current_basket[fruits[r]] += 1
-                result = max(result, r - l + 1)
-                r += 1
-            else:
-                # print(l, r, current_basket)
-                while len(current_basket) >= 2:
-                    current_basket[fruits[l]] -= 1
-                    if current_basket[fruits[l]] == 0:
-                        del current_basket[fruits[l]]
-                    l += 1
+            basket[fruits[r]] += 1
+            while len(basket) > 2:
+                basket[fruits[l]] -= 1
+                if basket[fruits[l]] == 0: basket.pop(fruits[l])
+                l += 1
+            result = max(result, r - l + 1)
+            r += 1
         return result
                 
                 
