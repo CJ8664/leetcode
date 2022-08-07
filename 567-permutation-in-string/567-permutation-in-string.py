071f3165eeddc9f5276285194bda85797ac40f07
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-07 11:54:24 -0700

    Time: 204 ms (33.87%), Space: 14 MB (31.78%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,37 +4,20 @@ class Solution:
             return False
         
         s1_counter = Counter(s1)
-        l, r = 0, 0
-        
+        s2_counter = Counter(s2[:len(s1)])
+        if s1_counter == s2_counter:
+            return True
+        l, r = 0, len(s1)
         while r < len(s2):
-            # Move r pointer forward till we find first occurrance 
-            # of any char in s1
-            if s2[r] not in s1_counter:
-                r += 1
-                continue
-            # We want to start tracking s1 from r so we
-            # have to bring l at same position as r
-            while l < r:
-                if s2[l] in s1_counter:
-                    s1_counter[s2[l]] += 1
-                l += 1
-            
-            # Window logic
-            while r < len(s2) and s2[r] in s1_counter:
-                # If the char at r is already used up 
-                # shrink window from left until we restore that char
-                while s1_counter[s2[r]] == 0:
-                    if s2[l] in s1_counter:
-                        s1_counter[s2[l]] += 1
-                    l += 1
-                # IF char at r is in s1 decrement its counter
-                s1_counter[s2[r]] -= 1
-                r += 1
-                
-                # IF the window size is len of s1 return true
-                if r - l == len(s1):
-                    return True
+            s2_counter[s2[r]] += 1
+            s2_counter[s2[l]] -= 1
+            if s2_counter[s2[l]] == 0:
+                s2_counter.pop(s2[l])
+            if s1_counter == s2_counter:
+                return True
+            l, r = l + 1, r + 1
         return False
+            
             
         
         
