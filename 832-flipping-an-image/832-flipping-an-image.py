9be10c96c429941275b37930f633beacbfb3fc89
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-08 20:45:14 -0700

    Time: 51 ms (95.35%), Space: 13.9 MB (65.73%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -6,7 +6,7 @@ class Solution:
                 image[i][l], image[i][r] = image[i][r], image[i][l]
                 l, r = l + 1, r - 1
             for j in range(len(image)):
-                image[i][j] = (image[i][j] + 1) % 2
+                image[i][j] ^= 1
         return image
         
         
