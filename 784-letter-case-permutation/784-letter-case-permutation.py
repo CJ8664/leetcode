d6ad20d2ebb3d9d8494edc673247d0c4e96cf7cc
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-09 21:58:04 -0700

    Time: 124 ms (18.49%), Space: 14.7 MB (81.20%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -6,10 +6,8 @@ class Solution:
             res = []
             for x in helper(idx + 1):
                 if s[idx].isalpha():
-                    res.append(s[idx] + x)
                     res.append(s[idx].swapcase() + x)
-                else:
-                    res.append(s[idx] + x)
+                res.append(s[idx] + x)
             return res
         return helper(0)
                     
