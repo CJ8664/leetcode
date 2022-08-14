b270fe1cac49c056bb86f8b007da88ba1ff01c49
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-14 15:30:49 -0700

    Time: 204 ms (82.86%), Space: 19.3 MB (11.29%) - LeetHub

---
+++ 554-brick-wall/554-brick-wall.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        # Init the dict with default values for the case where there is only
+        # one brick in each row of the wall
+        gap_count = {0: 0}
+        for row in wall:
+            gap = 0
+            for brick in row[:-1]:
+                gap += brick
+                gap_count[gap] = 1 + gap_count.get(gap, 0)
+        return len(wall) - max(gap_count.values())
+                
+            
+            
+            
+        
+        
