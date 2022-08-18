4fd2134c9d9cdb23b588f2eacf6996d696a21554
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-18 06:13:59 -0700

    Time: 320 ms (5.10%), Space: 14.2 MB (68.01%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,5 +1,14 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # High level idea is we want to form the left part of the combined
+        # array. For that partition small array such that we can pick elements
+        # from the left part and then based on the total and half number of elements
+        # in the combined array we pick remaining elements from the big array
+        # Hence we have to do multiple iterations where we find the perfect
+        # partition for small array using binary search
+        # Here the perfect partition is 
+        # small_left <= big_right and big_left <= small_right
+        
         small, big = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
         total = len(small) + len(big)
         half = total // 2
