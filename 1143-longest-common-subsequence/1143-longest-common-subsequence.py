95831b4a2dfcefa439d7d1508214ed9d15657ffa
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-24 17:58:11 -0700

    Time: 466 ms (83.57%), Space: 13.9 MB (98.22%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Good explanation: https://leetcode.com/problems/longest-common-subsequence/discuss/351689/JavaPython-3-Two-DP-codes-of-O(mn)-and-O(min(m-n))-spaces-w-picture-and-analysis
         if len(text1) > len(text2):
             # Text 1 should always be small
             return self.longestCommonSubsequence(text2, text1)
@@ -14,6 +15,7 @@ class Solution:
                     dp[curr_row][j] = max(dp[curr_row][j - 1], dp[prev_row][j])
             # Make the current state available in next iteration
             prev_row, curr_row = curr_row, prev_row
+        # Prev row because they are swapped after the iteration
         return dp[prev_row][-1]
                     
                 
