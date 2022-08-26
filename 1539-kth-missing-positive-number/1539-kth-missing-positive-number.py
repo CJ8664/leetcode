5a12364e2cd55f686a7249446496c508d67a47fe
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-26 10:49:07 -0700

    Time: 117 ms (21.55%), Space: 14 MB (86.45%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,21 +1,25 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        i, n = 0, 0
-        while i < len(arr):
-            n += 1
-            # If the arr element is not same as "n"
-            # decrement "k" to indicate that one missing 
-            # element is processed
-            if n != arr[i]:
-                k -= 1
-            # If the element is the same as "n" just move
-            # to the next element in array
+#         Assume the final result is x,
+#         And there are m number not missing in the range of [1, x].
+#         Binary search the m in range [0, A.size()].
+
+#         If there are m number not missing,
+#         that is A[0], A[1] .. A[m-1],
+#         the number of missing under A[m] is A[m] - 1 - m.
+
+#         If A[m] - 1 - m < k, m is too small, we update left = m.
+#         If A[m] - 1 - m >= k, m is big enough, we update right = m.
+
+#         Note that, we exit the while loop, l = r,
+#         which equals to the number of missing number used.
+#         So the Kth positive number will be l + k.
+        l, r = 0, len(arr)
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] - 1 - m < k:
+                l = m + 1
             else:
-                i += 1
-            if k == 0:
-                return n
-        # If all "k" missing elements were not processed in array
-        # n would be the last element in array + k remaining missing
-        # elements to be processed
-        return n + k
+                r = m
+        return l + k
         
