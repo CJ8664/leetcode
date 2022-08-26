614ddddd892fd73494b9c0fb6937e0a1a8687d1f
Chirag Jain <er.chiragjain92@gmail.com>
2022-08-26 11:18:14 -0700

    Time: 3196 ms (50.30%), Space: 59.2 MB (34.17%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -3,10 +3,13 @@ class Solution:
         res = [-1, -1, -1]
         tx, ty, tz = target
         for x, y, z in triplets:
+            # Fill the buckets till you have space If there 
+            # is no space left in any of the buckets then stop
             if x <= tx and y <= ty and  z <= tz:
                 res[0] = max(res[0], x)
                 res[1] = max(res[1], y)
                 res[2] = max(res[2], z)
+        # Check if all the buckets are filled
         return all([a==x for a, x in zip(target, res)])
             
             
